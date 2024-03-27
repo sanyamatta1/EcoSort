891a4b76512feb9e39f0d290435bd123169b0a7d
Sanya Matta <mattasanya1@gmail.com>
2024-03-27 16:21:51 -0700

    Initial Commit

---
+++ EcoSort/EcoSortApp.swift
@@ -0,0 +1,17 @@
+//
+//  EcoSortApp.swift
+//  EcoSort
+//
+//  Created by sanya matta on 3/27/24.
+//
+
+import SwiftUI
+
+@main
+struct EcoSortApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
